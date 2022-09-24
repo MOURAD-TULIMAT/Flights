659ee34e04b611ecf970cb8839d7b803f2b01cec
mourad tulimat <rmmr110@gmail.com>
2022-09-24 14:15:18 +0300

    add home and login and sign up

---
+++ flights/flights/wwwroot/js/layout.js
@@ -0,0 +1,23 @@
+﻿function Vlogin() {
+    document.getElementById("signup").style.display = "none";
+    const regblock = document.querySelector("#blur");
+    regblock.classList.add("blur");
+    document.getElementById("login").style.display = "flex";
+}
+function unVlogin() {
+    const regblock = document.querySelector("#blur");
+    regblock.classList.remove("blur");
+    document.getElementById("login").style.display = "none";
+}
+
+function Vsignup() {
+    document.getElementById("login").style.display = "none";
+    const regblock = document.querySelector("#blur");
+    regblock.classList.add("blur");
+    document.getElementById("signup").style.display = "flex";
+}
+function unVsignup() {
+    const regblock = document.querySelector("#blur");
+    regblock.classList.remove("blur");
+    document.getElementById("signup").style.display = "none";
+}
