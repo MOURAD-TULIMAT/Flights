985012fc06dd47afabbc0b5ada452104a44ef04e
mourad tulimat <rmmr110@gmail.com>
2022-09-27 08:09:44 +0300

    footer

--- flights/flights/wwwroot/js/layout.js
+++ flights/flights/wwwroot/js/layout.js
@@ -3,11 +3,14 @@
     const regblock = document.querySelector("#blur");
     regblock.classList.add("blur");
     document.getElementById("login").style.display = "flex";
+    window.scrollTo(0, 0);
+    disableScroll();
 }
 function unVlogin() {
     const regblock = document.querySelector("#blur");
     regblock.classList.remove("blur");
     document.getElementById("login").style.display = "none";
+    enableScroll();
 }
 
 function Vsignup() {
@@ -15,9 +18,53 @@ function Vsignup() {
     const regblock = document.querySelector("#blur");
     regblock.classList.add("blur");
     document.getElementById("signup").style.display = "flex";
+    window.scrollTo(0, 0);
+    disableScroll();
 }
 function unVsignup() {
     const regblock = document.querySelector("#blur");
     regblock.classList.remove("blur");
     document.getElementById("signup").style.display = "none";
+    enableScroll();
+}
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
