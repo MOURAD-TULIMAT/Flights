877858b74cae353c7e5f5888276a4b8bde35b30a
Marcel Ibrahim <marcel.ibrahim@wizme.com>
2022-10-02 08:21:29 +0300

    fix appsetting

--- flights/flights/Controllers/HomeController.cs
+++ flights/flights/Controllers/HomeController.cs
@@ -1,24 +1,33 @@
-﻿using flights.DTOs;
+﻿using flights.Data;
+using flights.DTOs;
 using flights.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Data.Entity;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace flights.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpGet]
         public IActionResult Offers()
         {
@@ -28,12 +37,40 @@ namespace flights.Controllers
         [HttpPost]
         public IActionResult Login(UserDto userDto)
         {
+            
             return RedirectToAction("", "Home");
         }
 
         [HttpPost]
-        public IActionResult Signup(UserDto userDto)
+        public async Task<IActionResult> SignupAsync(UserDto userDto)
         {
+            var accountTyoe = await _context
+                .AccountTypes.FirstAsync(a => a.Name == "User");
+
+            var account = new Account
+            {
+                Id = Guid.NewGuid(),
+                UserName = userDto.Email,
+                Password = userDto.Password,
+                TypeId = accountTyoe.Id,
+                CreationDate = DateTime.Now.ToShortDateString()
+            };
+
+           await  _context.Accounts.AddAsync(account);
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                AccountId = account.Id,
+                Name = $"{userDto.Fname} {userDto.Lname}",
+                Address = "",
+                Phone = "",
+            };
+
+            await _context.Users.AddAsync(user);
+
+            await _context.SaveChangesAsync();
+
             return RedirectToAction("", "Home");
         }
 
